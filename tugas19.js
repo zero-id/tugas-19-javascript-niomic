a94bc2678cb95f2a56c00a19cae5463961c554d7
zero-id <abdullatiffebriliana15@gmail.com>
2023-03-13 18:06:59 +0700

    tugas 19 js

---
+++ tugas19.js
@@ -0,0 +1,27 @@
+function tugas19() {
+    var x = "Semangat...";
+    console.log(x.repeat(5));
+    
+}
+
+tugas19()
+
+console.log("");
+
+
+function tugas192() {
+    var y = "*****";
+    
+    console.log(y);
+    console.log(y.repeat(5));
+    console.log(y.repeat(10));
+    console.log(y.repeat(15));
+    console.log(y.repeat(20));
+    console.log(y.repeat(15));
+    console.log(y.repeat(10));
+    console.log(y.repeat(5));
+    console.log(y);
+    
+}
+
+tugas192()
